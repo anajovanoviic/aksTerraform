845fd1c3d76a0d5bc4e1bd3f487a4dd7e745336f
anajovanoviic <51513732+anajovanoviic@users.noreply.github.com>
2022-08-16 11:54:07 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,1 +1,14 @@
+provider "azurerm" {
+  subscription_id = "891195e4-c78e-4b5d-86b4-88b6d1875195"
+  features {}
+}
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      //version = "2.39.0"
+      version = "3.6.0"
+    }
+  }
+}
